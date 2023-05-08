b22c937934ec68898ac52d94439b79daf6f5f0ba
marcadrian-it <marcadrian.it@gmail.com>
2023-05-08 21:46:20 +0200

    add db logging

--- index-ws.js
+++ index-ws.js
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
 server.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+process.on('SIGINT', ()=> {
+  wss.clients.forEach(function each(client){
+    client.close();
+  });
+  server.close(()=>{
+    shutdownDB();
+  })
+})
 /* Begin websocket */
 import { WebSocketServer } from 'ws';
 
@@ -27,6 +36,10 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my chat');
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+      VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close() {
         console.log('Client has disconnected');
         wss.broadcast(`Current visitors: ${numClients}`);
@@ -38,6 +51,28 @@ wss.on('connection', function connection(ws) {
                 client.send(data); 
     });
     };
+/**begin database **/
+import sqlite3 from 'sqlite3';
+const sqlite = sqlite3.verbose();
+const db = new sqlite.Database(':memory:');
 
+db.serialize(()=> {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `)
+});
 
+function getCounts(){
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  })
+}
 
+function shutdownDB(){
+  getCounts();
+  console.log('Shutting down db');
+  db.close();
+}
